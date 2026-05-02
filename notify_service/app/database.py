8786baaadcee2e251f58980c89b7eb3ac9f44359
Bogdan <lasenkob213@gmail.com>
2026-05-02 16:54:31 +0300

    Пробую уровень ci\cd

--- notify_service/app/database.py
+++ notify_service/app/database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Читаем URL из переменной окружения DATABASE_URL
 # Если переменной нет (запуск локально), используем localhost:5433
